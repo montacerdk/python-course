1897dbd99250b9d37fe6cb69b021820d39ee355e
Montacer Dkhilali <montacer.dkhilali@gmail.com>
2021-08-14 18:08:19 +0100

    Trying out some list functions

--- test_features.py
+++ test_features.py
@@ -3,3 +3,13 @@ obj = {'foo': 'titi', 'bar': None}
 has_bar = obj['foo'] or obj['foo']
 
 print('Result', has_bar)
+
+luck_numbers = [3, 6, 9, 12]
+friends = ['Hermano', 'Ghassen', 'Kais']
+
+# List Functions.
+luck_numbers.extend(friends)
+luck_numbers.append(100)
+luck_numbers.insert(5, "El Manifico")
+luck_numbers.remove(9)
+print("Lucky Numbers ... ", luck_numbers)
