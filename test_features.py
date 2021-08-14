916308a0f583455fe7d77ddd0135e43e5e369009
Montacer Dkhilali <montacer.dkhilali@gmail.com>
2021-08-14 20:40:37 +0100

    Working with Dictionnaries

--- test_features.py
+++ test_features.py
@@ -1,19 +1,41 @@
-obj = {'foo': 'titi', 'bar': None}
+# Dictionnary
 
-has_bar = obj['foo'] or obj['foo']
+months = {
+    'Jan': 'January',
+    'Feb': 'February',
+    'Mar': 'March',
+    'Apr': 'April',
+    'May': 'May',
+    'Jun': 'June',
+    'Jul': 'July',
+    'Aug': 'August',
+    'Sep': 'September',
+    'Oct': 'October',
+    'Nov': 'November',
+    'Dec': 'December'
+}
+
+print('Montacer is born in ' + months.get('Oct'))
+
+obj = {'Foo': 'titi', 'Bar': None}
+
+has_bar = obj['Foo'] or obj['Foo']
 
 print('Result', has_bar)
+
+# List Functions.
 
 luck_numbers = [3, 6, 9, 12]
 friends = ['Hermano', 'Ghassen', 'Kais']
 
-# List Functions.
 luck_numbers.extend(friends)
 luck_numbers.append(100)
 luck_numbers.insert(5, "El Manifico")
 luck_numbers.remove(9)
 print("Lucky Numbers ... ", luck_numbers)
 
+
+# Functions
 
 def say_hi(name):
     print('Hello ' + name + ', Ab3eth Ab3eth.')
@@ -25,6 +47,8 @@ def get_cube(number):
 
 say_hi('Walid')
 print(get_cube(6))
+
+# If statements
 
 is_female = True
 is_tall = True
